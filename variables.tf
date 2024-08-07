2f62aff0465df81fc76770be653564b178a32ded
Manikandan-Premkumar <113436747+Manikandan-Premkumar@users.noreply.github.com>
2024-08-07 22:36:04 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,5 @@
-variables "bucketname"{
-    constant = "staticswebsite2024"
-}
-
-resource "aws_route_53" "exampleDomain" {
-  name = var.domainName
+variable "bucketname" {
+  description = "Controls if S3 bucket should be created"
+  type        =  string
+  default     = "manistaticwebsite2024"
 }
